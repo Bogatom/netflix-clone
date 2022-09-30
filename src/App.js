e58a2812a5071370ef740d84af87ef7a4b3c1df3
Bogatom <tomvandenbogaart1@hotmail.com>
2022-09-30 14:50:31 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import './App.css';
 import Row from "./Row";
 import requests from './requests';
 import Banner from './Banner';
+import Nav from './Nav';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav />
       <Banner />
-      <Row 
-        title="NETFLIX ORIGINALS" 
-        fetchUrl={requests.fetchNetflixOriginals} 
-        isLargeRow
-      />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
       <Row title="Action" fetchUrl={requests.fetchActionMovies}/>
